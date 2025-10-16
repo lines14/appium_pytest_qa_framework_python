d23c4e5d194bbc65e06e66ebb4aba4df5a52b633
lines14 <tonybass1414@yahoo.com>
2025-10-17 01:15:45 +0500

    disable database test case due to lack of vpn tunneling from runner

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from tests.API.auth_API import AuthAPI
-from main.utils.DB.base_DB import BaseDB
+# from main.utils.DB.base_DB import BaseDB
 from main.utils.log.logger import Logger
 from main.driver.driver_utils import DriverUtils
 from main.driver.driver_factory import DriverFactory
@@ -8,13 +8,13 @@ from main.driver.driver_factory import DriverFactory
 
 @pytest.fixture(scope="function", autouse=True)
 async def setup_and_teardown():
-    base_DB = BaseDB()
-    await base_DB.init_tables()
+    # base_DB = BaseDB()
+    # await base_DB.init_tables()
     await AuthAPI().set_token()
     DriverUtils.init_the_driver()
 
     yield DriverFactory.instance
 
     DriverUtils.quit_driver()
-    await base_DB.dispose_engine()
+    # await base_DB.dispose_engine()
     Logger.log_to_file()
