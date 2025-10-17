c1f0a1fe1e8df110de65f047500956aa5d86a7f7
lines14 <tonybass1414@yahoo.com>
2025-10-17 20:12:24 +0500

    try github actions pipelines

--- tests/screen_objects/start_screen.py
+++ tests/screen_objects/start_screen.py
@@ -4,7 +4,7 @@ from main.elements.base_elements.button import Button
 
 class StartScreen(BaseScreen):
     def __init__(self):
-        super().__init__(AppiumBy.XPATH, '//android.widget.TextView[contains(@text, "Добро пожаловать")]', 'start screen')
+        super().__init__(AppiumBy.XPATH, '//*[contains(@text, "Добро пожаловать")]', 'start screen') # compatible with older api-levels
         self.login_button = Button(AppiumBy.XPATH, '//android.widget.Button[@text="Вход"]', 'login button')
         self.by_email_button = Button(AppiumBy.XPATH, "//button[contains(., 'По email')]", 'by email button')
 
